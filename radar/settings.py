105a03907233dc633de72e9b0ad89862340d722a
Andrew Brookins <a.m.brookins@gmail.com>
2014-09-14 13:50:37 -0700

    Move login link to the top navbar

--- radar/settings.py
+++ radar/settings.py
@@ -125,6 +125,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
